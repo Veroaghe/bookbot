cc3cb59e26bc8d46e49f6954e284c59bcb776802
Glenn Verhaeghe <veroaghe@gmail.com>
2025-02-20 05:31:02 +0100

    added create_report function

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 def main():
     text_file = "books/frankenstein.txt"
-    text = get_text_contents(text_file)
-    char_dict = count_characters(text)
-    print(char_dict)
+    create_report(text_file)
     
 
 def get_text_contents(text_file):
@@ -26,6 +24,25 @@ def count_characters(text):
             char_dict[char] += 1
 
     return char_dict
+
+
+def in_alphabet(char):
+    return ord(char.lower()) in range(ord('a'), ord('z') + 1)
+
+
+def create_report(text_file):
+    text = get_text_contents(text_file)
+    char_dict = count_characters(text)
+    word_count = count_words(text)
+
+    print(f"--- Begin report of {text_file} ---")
+    print(f"{word_count} words found in the document\n")
+
+    for char in char_dict:
+        if in_alphabet(char):
+            print(f"The '{char}' character was found {char_dict[char]} times")
+
+    print("--- End report ---")
 
 
 main()
